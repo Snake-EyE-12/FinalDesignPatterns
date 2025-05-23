5556d50682601e6e6531da181d9e854f05a1b8b7
Chase Guymon <chase.guymon@q.com>
2025-05-22 18:20:25 -0600

    Update GameMakerMain.cs

--- GameMakerMain.cs
+++ GameMakerMain.cs
@@ -8,20 +8,57 @@ namespace FinalProjectPSD
 {
     internal class GameMakerMain
     {
+        IInputReader inputReader = new InputReader();
         public void Run()
+        {
+            List<Command> commands = new List<Command>();
+            bool gameRunning = true;
+            while(gameRunning)
+            {
+                commands.Add(GetAndRunIdea());
+                gameRunning = GetShouldContinue();
+            }
+            Console.WriteLine("\n");
+            commands.ForEach(command => command.Execute());
+        }
+        private bool GetShouldContinue()
+        {
+            Console.WriteLine("\n");
+            bool cont = inputReader.GetBool("Continue?");
+            Console.WriteLine("\n");
+            return cont;
+        }
+        private Command GetAndRunIdea()
         {
             Console.WriteLine("\n\n\n");
             Console.WriteLine("Initializing Generation");
-            IInputReader inputReader = new InputReader();
             string inputtedStrategyType = inputReader.GetString("Input Genre");
             IIdeaGeneratorFactory factory = new ConcreteFactory();
             IdeaGenerationStrategy genreStrategy = factory.GetStrategy(inputtedStrategyType);
             Console.WriteLine(genreStrategy.Generate());
-
-
             Console.WriteLine("\n\n\n\n\n\n\n");
+            return new LoggerCommand(genreStrategy);
         }
     }
+    public interface Command
+    {
+        public void Execute();
+    }
+    public class LoggerCommand : Command
+    {
+        IdeaGenerationStrategy generator;
+        public LoggerCommand(IdeaGenerationStrategy selectedGenerator)
+        {
+            generator = selectedGenerator;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("Logging... Idea Generator: " + generator.GetType() + " | Loaded-Successfully");
+        }
+    }
+
+
     public interface IIdeaGenerator
     {
         public void LoadStrategy(IdeaGenerationStrategy strategy);
@@ -38,6 +75,14 @@ namespace FinalProjectPSD
             return "Platformer Idea";
         }
     }
+    public class ConreteActionIdeaGenerationStrategy : IdeaGenerationStrategy { public string Generate() { return "Action Game Idea"; } }
+    public class ConreteFPSIdeaGenerationStrategy : IdeaGenerationStrategy { public string Generate() { return "FPS Idea"; } }
+    public class ConretePuzzleIdeaGenerationStrategy : IdeaGenerationStrategy { public string Generate() { return "Puzzle Concept"; } }
+    public class ConreteRogueliteIdeaGenerationStrategy : IdeaGenerationStrategy { public string Generate() { return "Rogue but lite"; } }
+    public class ConreteRoguelikeIdeaGenerationStrategy : IdeaGenerationStrategy { public string Generate() { return "Rogue but like"; } }
+    public class ConreteRPGIdeaGenerationStrategy : IdeaGenerationStrategy { public string Generate() { return "RPG Idea"; } }
+    public class ConreteRTSIdeaGenerationStrategy : IdeaGenerationStrategy { public string Generate() { return "RTS Idea"; } }
+    public class ConreteTDIdeaGenerationStrategy : IdeaGenerationStrategy { public string Generate() { return "Only Bloons TD"; } }
     public interface IIdeaGeneratorFactory
     {
         public IdeaGenerationStrategy GetStrategy(string method);
@@ -48,7 +93,15 @@ namespace FinalProjectPSD
         {
             switch(method)
             {
-                case "Platformer": return new ConcretePlatformerIdeaGenerationStrategy();
+                case "Platformer":      return new ConcretePlatformerIdeaGenerationStrategy();
+                case "Action":          return new ConreteActionIdeaGenerationStrategy();
+                case "FPS":             return new ConreteFPSIdeaGenerationStrategy();
+                case "Puzzle":          return new ConcretePlatformerIdeaGenerationStrategy();
+                case "Roguelite":       return new ConreteRogueliteIdeaGenerationStrategy();
+                case "Roguelike":       return new ConreteRoguelikeIdeaGenerationStrategy();
+                case "RPG":             return new ConreteRPGIdeaGenerationStrategy();
+                case "RTS":             return new ConreteRTSIdeaGenerationStrategy();
+                case "TowerDefense":    return new ConreteTDIdeaGenerationStrategy();
                 default: return null;
             }
         }
@@ -56,8 +109,9 @@ namespace FinalProjectPSD
     public interface IInputReader
     {
       
-        public int GetInt(String message);
-        public string GetString(String message);
+        public int GetInt(string message);
+        public string GetString(string message);
+        public bool GetBool(string message);
     }
     public class InputReader : IInputReader
     {
@@ -70,6 +124,46 @@ namespace FinalProjectPSD
         {
             Console.WriteLine(message);
             return GetNonEmptyValue();
+        }
+        public bool GetBool(string message)
+        {
+            Console.WriteLine(message);
+            bool valid = false;
+            bool answer = false;
+            while(!valid)
+            {
+                string typed = GetNonEmptyValue();
+                
+                switch(typed)
+                {
+                    case "true":
+                    case "True":
+                    case "TRUE":
+                    case "yes":
+                    case "y":
+                    case "Yes":
+                    case "Y":
+                    case "YES":
+                        answer = true;
+                        valid = true;
+                        break;
+                    case "false":
+                    case "False":
+                    case "FALSE":
+                    case "no":
+                    case "n":
+                    case "No":
+                    case "NO":
+                        answer = false;
+                        valid = true;
+                        break;
+                    default:
+                        answer = false;
+                        valid = false;
+                        break;
+                }
+            }
+            return answer;
         }
         private string GetNonEmptyValue()
         {
